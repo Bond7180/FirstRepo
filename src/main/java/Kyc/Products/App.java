0893f20d4d93ce7e69723cc8e269a526ba872415
unknown <swapnil@vtestcorp.com>
2021-07-31 23:36:03 +0530

    N

---
+++ src/main/java/Kyc/Products/App.java
@@ -0,0 +1,13 @@
+package Kyc.Products;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
